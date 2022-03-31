0a999daf5e8b1c72aa71a9512a088f064905391e
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-03-30 21:11:19 -0300

    Ejercicio 1

---
+++ Assets/Scripts/ej1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej1 : MonoBehaviour {
+
+    /*Realizá un programa donde se cree una variable entera llamada num1, que inicialmente  valdrá 0. 
+     * Luego*/
+    //incrementá su valor en 2 y mostralo por pantalla.Después mostrá el  resultado de multiplicarlo por
+    //sí mismo. 
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        int num1 = 0;
+        num1 += 2;
+        Debug.Log(num1);
+        Debug.Log(num1 * num1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
